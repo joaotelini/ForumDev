50fe4c7d80fc2ff0db58ea50f440e7a5ad607ef8
João Pedro Telini <joaotelini03@gmail.com>
2025-10-17 17:16:48 -0300

    feat: add Swagger UI integration for API documentation and update dependencies

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,27 @@ import helmet from "helmet";
 import routes from "./routes";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import fs from "fs";
+import yaml from "yaml";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
 
 const server = express();
 
+const file = fs.readFileSync(__dirname + "/docs/swagger.yaml", "utf8");
+const swaggerDocument = yaml.parse(file);
+
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 server.use(cookieParser());
+
 server.use(
   cors({
     origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
     credentials: true,
   })
 );
+
 server.use(helmet());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
